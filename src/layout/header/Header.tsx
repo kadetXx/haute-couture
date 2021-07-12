4a6bc71a7fe676b49bc38529c80885394cbedecf
kadet <techkadet@gmail.com>
2021-07-12 11:49:24 +0100

    completed hamburger menu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,17 +1,32 @@
-import React from "react";
-import './Header.scss'
+import React, { useState } from "react";
+import "./Header.scss";
+
 const Header = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
   return (
     <header>
       <div className="logo">Haute Couture</div>
 
-      <button className="hamburger">
-        <span className="hamburger__bar"></span>
-        <span className="hamburger__bar"></span>
-      </button>
-      {/* <nav>
-        
-      </nav> */}
+      <nav>
+        <ul className={showMenu ? "menu" : "menu menu--hidden"}>
+          <li className="menu__item">
+            <a href="#" className="menu__link">
+              TW.
+            </a>
+          </li>
+          <li className="menu__item">
+            <a href="#" className="menu__link">
+              GH.
+            </a>
+          </li>
+        </ul>
+
+        <button className={showMenu ? "hamburger hamburger--expanded" : "hamburger"} onClick={() => setShowMenu(!showMenu)}>
+          <span className="hamburger__bar"></span>
+          <span className="hamburger__bar"></span>
+        </button>
+      </nav>
     </header>
   );
 };
