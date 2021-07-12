f0f0939dd8a3eb25c687371f264290f2cd66b5dc
kadet <techkadet@gmail.com>
2021-07-12 13:36:57 +0100

    improvements on animations

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -3,28 +3,52 @@ import "./Featured.scss";
 
 import TitleBlock from "../titleBlock/TitleBlock";
 
+import FeaturedOne from "../../assets/images/featured-one.png";
+import FeaturedTwo from "../../assets/images/featured-two.png";
+import FeaturedThree from "../../assets/images/featured-three.png";
+import FeaturedFour from "../../assets/images/featured-four.png";
+
+interface FeaturedItem {
+  name: string;
+  image: string;
+}
+
 const Featured = () => {
+  const ladiesWear: FeaturedItem[] = [
+    {
+      name: "Lucid Dreams",
+      image: FeaturedOne,
+    },
+    {
+      name: "Magnificient",
+      image: FeaturedTwo,
+    },
+    {
+      name: "Stepper Girl",
+      image: FeaturedThree,
+    },
+    {
+      name: "Gram Attire",
+      image: FeaturedFour,
+    },
+  ];
+
   return (
     <div className="featured">
       <TitleBlock subtitle="Featured" title="Ladies Summer Collection" />
       <div className="featured__grid">
-        <div className="featured__item featured__item--one">
-          <p className="featured__item-label">Lucid Dreams</p>
-        </div>
-        <div className="featured__item featured__item--two">
-          <p className="featured__item-label">Ocean eyes</p>
-        </div>
-        <div className="featured__item featured__item--three">
-          <p className="featured__item-label">Lucid dreams</p>
-        </div>
-        <div className="featured__item featured__item--four">
-          <p className="featured__item-label">Ocean eyes</p>
-        </div>
+        {ladiesWear.map((item, index) => (
+          <div
+            key={item.name + index}
+            className="featured__item featured__item--one"
+            style={{ backgroundImage: `url('${item.image}')` }}
+          >
+            <p className="featured__item-label">{item.name}</p>
+          </div>
+        ))}
       </div>
       <div className="featured__cta">
-        <div className="btn">
-          View All
-        </div>
+        <div className="btn">View All</div>
       </div>
     </div>
   );
